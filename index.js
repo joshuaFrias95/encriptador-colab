21a6fca5d969a8f228a05630d66aa468b7691e7f
Joshua Frías <metatron1495@gmail.com>
2023-05-19 20:24:47 -0600

    Refactor: refactorizacion de JS

--- index.js
+++ index.js
@@ -1,51 +1,57 @@
+const texto = document.getElementById("texto").value;
+const tituloMensaje = document.getElementById("titulo-mensaje");
+const parrafo = document.getElementById("parrafo");
+const resultado = document.getElementById("resultado");
+const muñeco = document.getElementById("muñeco");
+
+let textoCifrado;
+
 function encriptar() {
-  let texto = document.getElementById("texto").value;
-  let tituloMensaje = document.getElementById("titulo-mensaje");
-  let parrafo = document.getElementById("parrafo");
-  let muñeco = document.getElementById("muñeco");
-
-  let textoCifrado = texto
-    .replace(/e/gi, "enter")
-    .replace(/i/gi, "imes")
-    .replace(/a/gi, "ai")
-    .replace(/o/gi, "ober")
-    .replace(/u/gi, "ufat");
-
+  
+  textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+    
   if (texto.length != 0) {
-    document.getElementById("texto").value = textoCifrado;
-    tituloMensaje.textContent = "Texto encriptado con éxito";
-    parrafo.textContent = "";
-    muñeco.src ="./img/encriptado.jpg";   
-  } else {
-    muñeco.src ="./img/muñeco2.png";
-    tituloMensaje.textContent = "Ningun mensaje fue encontrado";
-    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-    alert("Debes ingresar algún texto");
-  }
-}
 
-function desencriptar() {
-  let  texto = document.getElementById("texto").value;
-  let tituloMensaje = document.getElementById("titulo-mensaje");
-  let parrafo = document.getElementById("parrafo");
-  let muñeco = document.getElementById("muñeco");
-
-  let textoCifrado = texto
-    .replace(/enter/gi, "e")
-    .replace(/imes/gi, "i")
-    .replace(/ai/gi, "a")
-    .replace(/ober/gi, "o")
-    .replace(/ufat/gi, "u");
-
-    if (texto.length != 0) {
-      document.getElementById("texto").value = textoCifrado;
-      tituloMensaje.textContent = "Texto desencriptado con éxito";
+      tituloMensaje.textContent = "Texto encriptado con éxito";
+      resultado.textContent = textoCifrado;
       parrafo.textContent = "";
-      muñeco.src ="./img/desencriptado.jpg";   
-    }  else {
-      muñeco.src ="./img/muñeco2.png";
+      muñeco.src = "./img/encriptado.jpg";
+
+      console.log(textoCifrado);
+
+    } else {
+
+      muñeco.src ="./img/Muñeco2.png";
       tituloMensaje.textContent = "Ningun mensaje fue encontrado";
       parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
       alert("Debes ingresar algún texto");
+
     }
+}
+
+function desencriptar() {
+
+  textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");  
+
+  if (texto.length != 0) {
+
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+    resultado.textContent = textoCifrado;
+    parrafo.textContent = "";
+    muñeco.src = "./img/encriptado.jpg";
+
+    console.log(textoCifrado);
+
+  } else {
+
+    muñeco.src ="./img/Muñeco2.png";
+    tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    alert("Debes ingresar algún texto");
+
   }
+}
+
+function clipboard() {
+  
+}
